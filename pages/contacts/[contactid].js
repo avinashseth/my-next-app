581e8611c1b1c5a86950162b045fbffcc4cc3a59
Avinash Seth <bloggerbeg@gmail.com>
2022-10-10 11:52:48 +0530

    next js

--- pages/contacts/[contactid].js
+++ pages/contacts/[contactid].js
@@ -1,1 +1,17 @@
-// import Layout from '../../com'
+import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
+import contacts from "../api/contacts";
+import React from "react";
+
+export default function Contact() {
+
+    const router = useRouter();
+    const contact = contacts.find(contact => contact.id === router.query.contactid);
+    const [first, last] = contact?.name.split(" ");
+
+    return (
+        <>
+            <h1>Hello { last.toUpper() }</h1>
+        </>
+    )
+}
